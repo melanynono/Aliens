be84c18ae9217b27184d85e75d3bf34743cf8eec
MelanyNono <melanynono123@gmail.com>
2026-09-01 22:49:23 -0500

    Crear pruebas para validar precioTotal

--- src/com/krakedev/aliens/testJUnit/TestPruebasExtremidades.java
+++ src/com/krakedev/aliens/testJUnit/TestPruebasExtremidades.java
@@ -53,4 +53,64 @@ public class TestPruebasExtremidades {
         assertEquals(6, a1.getNumeroBrazos());
         assertEquals(0, a1.getNumeroPies());
     }
+    
+    @Test
+    public void agregarBrazosCorrectamente() {
+        Alien a1 = new Alien(10, "Verde");
+
+        boolean resultado = a1.agregarBrazos(4);
+
+        assertTrue(resultado);
+        assertEquals(4, a1.getNumeroBrazos());
+    }
+    
+    @Test
+    public void agregarPiesCorrectamente() {
+        Alien a1 = new Alien(10, "Verde");
+
+        boolean resultado = a1.agregarPies(3);
+
+        assertTrue(resultado);
+        assertEquals(3, a1.getNumeroPies());
+    }
+    
+    @Test
+    public void noMasDe10Extremidades() {
+        Alien a1 = new Alien(10, "Verde");
+
+        a1.agregarBrazos(6);
+
+        boolean resultado = a1.agregarPies(5);
+
+        assertFalse(resultado);
+        assertEquals(6, a1.getNumeroBrazos());
+        assertEquals(0, a1.getNumeroPies());
+    }
+    
+    @Test
+    public void noExcedeLimiteOjos() {
+        Alien a1 = new Alien(10, "Verde");
+
+        a1.agregarOjos(3);
+
+        boolean resultado = a1.agregarOjos(1);
+
+        assertFalse(resultado);
+        assertEquals(3, a1.getNumeroOjos());
+    }
+    
+    @Test
+    public void agregarElementosCalcularPrecio() {
+        Alien a1 = new Alien(20, "Azul");
+
+        a1.agregarBrazos(4);
+        a1.agregarPies(2);
+        a1.agregarOjos(5);
+
+        assertEquals(4, a1.getNumeroBrazos());
+        assertEquals(2, a1.getNumeroPies());
+        assertEquals(5, a1.getNumeroOjos());
+
+        assertEquals(21, a1.getPrecioTotal(), 0.001);
+    }
 }
