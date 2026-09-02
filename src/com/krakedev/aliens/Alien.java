c0c52d8a0feaab86448094f56f6892352f9f75f6
MelanyNono <melanynono123@gmail.com>
2026-09-01 19:00:12 -0500

    Agregar metodo imprimir

--- src/com/krakedev/aliens/Alien.java
+++ src/com/krakedev/aliens/Alien.java
@@ -67,4 +67,17 @@ public class Alien {
 		return precioCuerpo;
 	}
 	
+	public void imprimir() {
+		
+        System.out.println("Tamaño: " + tamanio);
+        System.out.println("Color: " + color);
+        System.out.println("Número de ojos: " + numeroOjos);
+        System.out.println("Número de brazos: " + numeroBrazos);
+        System.out.println("Número de pies: " + numeroPies);
+        System.out.println("Precio extremidad: " + precioExtremidad);
+        System.out.println("Precio ojo: " + precioOjo);
+        System.out.println("Precio cuerpo: " + precioCuerpo);
+	}
+	
+	
 }
